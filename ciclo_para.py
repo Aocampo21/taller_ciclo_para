cd304dcba5498d999933f9a6e2e664510cfb1f72
Amanda Ocampo <aocampo@cuc.edu.co>
2021-09-19 18:31:18 -0500

    Ejercicio 10

--- ciclo_para.py
+++ ciclo_para.py
@@ -218,12 +218,44 @@ De 160 Millones en adelante - 30%"""
 for i in range(0, 100):
         venta = int(input("Digite cuanto vendió el empleado número "+ str(i)+" en millones de pesos "))
         if(venta < 20):
-            print("La comisión del empleado " + str(i)+"fue de ", venta*0.1)
+            print("La comisión del empleado " + str(i)+" fue de ", venta*0.1)
         if(venta < 40 and venta > 20):
-            print("La comisión del empleado " + str(i)+"fue de ", venta*0.15)
+            print("La comisión del empleado " + str(i)+" fue de ", venta*0.15)
         if(venta < 80 and venta >= 40):
-            print("La comisión del empleado " + str(i)+"fue de ", venta*0.20)
+            print("La comisión del empleado " + str(i)+" fue de ", venta*0.20)
         if(venta < 160 and venta >= 80):
-            print("La comisión del empleado " + str(i)+"fue de ", venta*0.25)
+            print("La comisión del empleado " + str(i)+" fue de ", venta*0.25)
         if(venta > 160):
-            print("La comisión del empleado " + str(i)+"fue de ", venta*0.30)
+            print("La comisión del empleado " + str(i)+" fue de ", venta*0.30)
+
+""" La empresa Encuestas S.A desea crear una función que les permita
+conocer de los 50.000 votos obtenidos por 3 candidatos, cual de estos fue
+el ganador o indicar si hubo empate y la cantidad de votos obtenidos."""
+
+candidato1 = 0
+candidato2 = 0
+candidato3 = 0
+ganador = 0
+
+for i in range(0, 50000):
+    voto = int(input('¿Por cuál candidato votó? 1,2 o 3? '))
+    if voto == 1:
+        candidato1 += 1
+    if voto == 2:
+        candidato2 += 1
+    if voto == 3:
+        candidato3 += 1
+
+if candidato1 > candidato2 and candidato1 > candidato3:
+    ganador = 1
+if candidato2 > candidato1 and candidato2 > candidato3:
+    ganador = 2
+if candidato3 > candidato1 and candidato3 > candidato2:
+    ganador = 3
+
+print('El primer candidato recibio', candidato1, 'votos')
+print('El segundo candidato recibio', candidato2, 'votos')
+print('El tercer candidato recibio', candidato3, 'votos')
+print("El ganador fue el candidato", ganador)
+
+
