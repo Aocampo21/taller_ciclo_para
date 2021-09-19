74a8200f712b4a210da4f2377aa70a5a84f73794
Amanda Ocampo <aocampo@cuc.edu.co>
2021-09-19 16:59:02 -0500

    Ejercicio 6

--- ciclo_para.py
+++ ciclo_para.py
@@ -116,3 +116,23 @@ for i in range(0, numeros-1):
         menor = num
 
 print("El número menor es: ", menor)
+
+""" Cinco miembros de un club contra la obesidad desean saber cuanto han
+bajado o subido de peso desde la última vez que se reunieron. Para esto se
+debe realizar un ritual de pesaje en donde cada uno se pesa en diez
+básculas distintas para así tener el pormedio mas exacto de su peso.
+Si existe diferencia positiva entre este promedio de peso y el peso de la
+última vez que se reunieron, significa que subieron de peso. Pero si la
+diferencia es negativa, significa que bajaron. Lo que el problema requere es
+que por cada persona se imprima un letrero que diga: “SUBIÓ” o “BAJÓ” y la
+cantidad de kilos que subió o bajó de peso."""
+
+peso = 0
+for i in range(1, 6):
+    peso_anterior = int((input("Digite el peso anterior del miembro n°" + str(i)+": ")))
+    for j in range(1, 11):
+     peso += int((input("Digite el peso de la báscula n°"+ str(j)+ " del miembro " + str(i)+": ")))     
+    if(peso_anterior-peso < 0):
+     print("El miembro bajó: ", peso_anterior - (peso/10), " kilos.")
+    if(peso_anterior-peso > 0):
+     print("El miembro subió: ", peso_anterior - (peso/10), " kilos.")
